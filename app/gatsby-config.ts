1d3100556aa34fbe4f4204aa38133a75a9770a73
hijikiman <30966790+hijikiman@users.noreply.github.com>
2022-12-02 16:39:45 +0900

    [add] format some ts files

--- app/gatsby-config.ts
+++ app/gatsby-config.ts
@@ -1,34 +1,43 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `hokora`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-TNLVX8C5Y3"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `hokora`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: 'G-TNLVX8C5Y3',
+            },
+        },
+        'gatsby-plugin-image',
+        'gatsby-plugin-sitemap',
+        'gatsby-plugin-mdx',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+    ],
+}
 
-export default config;
+export default config
